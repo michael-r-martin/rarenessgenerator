7c04083c24d875360edc3ec9a1718855283c4a2a
neurify <michael@neurify.co.uk>
2022-06-21 11:59:12 +0100

    Refactor for dynamic image adding

--- main.py
+++ main.py
@@ -1,26 +1,78 @@
 from PIL import Image
 import random
+import glob
+import os
+
+
+class LayerImages:
+    layer1Images = []
+    layer2Images = []
+    layer3Images = []
+    layer4Images = []
+    layer5Images = []
+
+
+numberLayers = 5
+
+
+def get_layer_images(layer):
+    image_folder = "images/"
+
+    layer_folder_path = f'{image_folder}{layer}/'
+
+    image_array = []
+
+    for x in os.listdir(layer_folder_path):
+        image_path = os.path.basename(x)
+
+        if image_path.__contains__(".png"):
+            image = Image.open(image_path)
+            image_array.append(image)
+
+    if layer == "layer1Images":
+        LayerImages.layer1Images = image_array
+    elif layer == "layer2Images":
+        LayerImages.layer2Images = image_array
+    elif layer == "layer3Images":
+        LayerImages.layer3Images = image_array
+    elif layer == "layer4Images":
+        LayerImages.layer4Images = image_array
+    elif layer == "layer5Images":
+        LayerImages.layer5Images = image_array
+
+
+for i in range(1, numberLayers):
+    get_layer_images(f'layer{i}Images')
+
+print(f'1 images: {LayerImages.layer1Images}')
+print(f'2 images: {LayerImages.layer2Images}')
+print(f'3 images: {LayerImages.layer3Images}')
+
+# import folder
+# for loop of folder and get all names of images
+# append to array
+# define number of layers and layer order
 
 whiteBG = Image.open("whiteBG.png")
 albaBG = Image.open("albaBG.png")
 mintGB = Image.open("mintBG.png")
 babyBlueBG = Image.open("babyBlueBG.png")
 bgs = [whiteBG, albaBG, mintGB, babyBlueBG]
-bgsArrayCount = len(bgs)-1
+bgsArrayCount = len(bgs) - 1
 
 whiteSmallCircle = Image.open("whiteSmallCircle.png")
 albaSmallCircle = Image.open("albaSmallCircle.png")
 mintSmallCircle = Image.open("mintSmallCircle.png")
 babyBlueSmallCircle = Image.open("babyBlueSmallCircle.png")
 smallCircles = [whiteSmallCircle, albaSmallCircle, mintSmallCircle, babyBlueSmallCircle]
-smallCirclesArrayCount = len(smallCircles)-1
+smallCirclesArrayCount = len(smallCircles) - 1
 
 whiteBigCircle = Image.open("whiteBigCircle.png")
 albaBigCircle = Image.open("albaBigCircle.png")
 mintBigCircle = Image.open("mintBigCircle.png")
 babyBlueBigCircle = Image.open("babyBlueBigCircle.png")
 bigCircles = [whiteBigCircle, albaBigCircle, mintBigCircle, babyBlueBigCircle]
-bigCirclesArrayCount = len(bigCircles)-1
+bigCirclesArrayCount = len(bigCircles) - 1
 
 createdImages = []
 
@@ -58,4 +110,4 @@ for i in range(100):
         iString = str(i)
 
         createdImages.append(createdImage)
-        newImage.save(f"{iString}.png")
+        # newImage.save(f"{iString}.png")
